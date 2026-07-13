3d8452c43918fcc04e34d32eb0e865782b3714d2
dwaris <dwaris@itclowd.de>
2026-07-13 18:00:25 +0200

    chore: run formatter

--- flake.nix
+++ flake.nix
@@ -5,20 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }@inputs: 
-  let
+  outputs = {nixpkgs, ...}: let
     systems = [
       "x86_64-linux"
       "aarch64-linux"
     ];
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
-  in
-  {
-    packages = forAllSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        manatan = pkgs.callPackage ./manatan.nix { };
+  in {
+    packages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        manatan = pkgs.callPackage ./manatan.nix {};
       in {
         default = manatan;
       }
--- manatan.nix
+++ manatan.nix
@@ -1,4 +1,4 @@
-{ 
+{
   stdenv,
   fetchurl,
   libgcc,
@@ -7,106 +7,100 @@
   buildFHSEnv,
   makeDesktopItem,
   copyDesktopItems,
-}:
+}: let
+  pkg = stdenv.mkDerivation (finalAttrs: {
+    pname = "manatan";
+    version = "6.0.24";
 
-let
-
-pkg = stdenv.mkDerivation (finalAttrs: {
-  pname = "manatan";
-  version = "6.0.24";
-
-  src = 
-  let
-    selectSystem =
-      attrs:
-      attrs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
-    system = selectSystem {
-      x86_64-linux = "amd64";
-      aarch64-linux = "arm64";
-    };
-  in
-    fetchurl {
-      url = "https://github.com/KolbyML/Manatan/releases/download/v${finalAttrs.version}/Manatan-v${finalAttrs.version}-Linux-${system}.tar.gz";
-      sha256 = selectSystem {
-        x86_64-linux = "sha256-/9cv6FC1nb2MVRFDmJ/upbSOTz/PBkrGcfyCGDoK+kQ=";
-        aarch64-linux = "sha256-40pRAVY7XLGnpgKSNQfkCnlwyJ+4NTNPB0oX/ASSH0E=";
+    src = let
+      selectSystem = attrs:
+        attrs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+      system = selectSystem {
+        x86_64-linux = "amd64";
+        aarch64-linux = "arm64";
       };
+    in
+      fetchurl {
+        url = "https://github.com/KolbyML/Manatan/releases/download/v${finalAttrs.version}/Manatan-v${finalAttrs.version}-Linux-${system}.tar.gz";
+        sha256 = selectSystem {
+          x86_64-linux = "sha256-/9cv6FC1nb2MVRFDmJ/upbSOTz/PBkrGcfyCGDoK+kQ=";
+          aarch64-linux = "sha256-40pRAVY7XLGnpgKSNQfkCnlwyJ+4NTNPB0oX/ASSH0E=";
+        };
+      };
+
+    buildInputs = [
+      libgcc
+    ];
+
+    sourceRoot = ".";
+
+    installPhase = ''
+      runHook preInstall
+      install -m755 -D manatan $out/bin/manatan
+      runHook postInstall
+    '';
+
+    nativeBuildInputs = [copyDesktopItems];
+
+    desktopItems = [
+      (makeDesktopItem {
+        name = "manatan";
+        exec = "manatan";
+        comment = "Seamless immersion language learning for anime, manga, novels on all platforms";
+        desktopName = "Manatan";
+        categories = ["Education"];
+      })
+    ];
+
+    meta = {
+      homepage = "https://manatan.com";
+      description = "Seamless immersion language learning for anime, manga, novels on all platforms";
     };
-
-  buildInputs = [
-    libgcc
-  ];
-
-  sourceRoot = ".";
-
-  installPhase = ''
-    runHook preInstall
-    install -m755 -D manatan $out/bin/manatan
-    runHook postInstall
-  '';
-
-  nativeBuildInputs = [ copyDesktopItems ];
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "manatan";
-      exec = "manatan";
-      comment = "Seamless immersion language learning for anime, manga, novels on all platforms";
-      desktopName = "Manatan";
-      categories = [ "Education" ];
-    })
-  ];
-
-  meta = {
-    homepage = "https://manatan.com";
-    description = "Seamless immersion language learning for anime, manga, novels on all platforms";
-  };
-});
-
+  });
 in
+  buildFHSEnv {
+    inherit (pkg) pname version meta;
 
-buildFHSEnv {
-  inherit (pkg) pname version meta;
+    extraInstallCommands = ''
+      mkdir -p $out/share/applications
+      cp -r ${pkg}/share/applications/* $out/share/applications/
+    '';
 
-  extraInstallCommands = ''
-    mkdir -p $out/share/applications
-    cp -r ${pkg}/share/applications/* $out/share/applications/
-  '';
+    runScript = "${pkg.outPath}/bin/manatan";
 
-  runScript = "${pkg.outPath}/bin/manatan";
-
-  targetPkgs = pkgs: with pkgs; [
-    alsa-lib
-    at-spi2-core
-    cairo
-    cups
-    dbus
-    expat
-    fontconfig
-    freetype
-    gdk-pixbuf
-    glib
-    gtk3
-    harfbuzz
-    libGL
-    libepoxy
-    libgbm
-    libx11
-    libxcb
-    libxcomposite
-    libxdamage
-    libxext
-    libxfixes
-    libxi
-    libxkbcommon
-    libxrandr
-    libz
-    lsof
-    mpv
-    nspr
-    nss
-    pango
-    udev
-    wayland
-  ];
-}
+    targetPkgs = pkgs:
+      with pkgs; [
+        alsa-lib
+        at-spi2-core
+        cairo
+        cups
+        dbus
+        expat
+        fontconfig
+        freetype
+        gdk-pixbuf
+        glib
+        gtk3
+        harfbuzz
+        libGL
+        libepoxy
+        libgbm
+        libx11
+        libxcb
+        libxcomposite
+        libxdamage
+        libxext
+        libxfixes
+        libxi
+        libxkbcommon
+        libxrandr
+        libz
+        lsof
+        mpv
+        nspr
+        nss
+        pango
+        udev
+        wayland
+      ];
+  }
